6fd2aa2ac6f9a67b3267741f7fbad09638be7f9e
Tim <timurnuriev@bk.ru>
2022-10-17 13:14:17 +0500

    added code

--- task/Program.cs
+++ task/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿Console.Clear();
+string[] array = { "hello", "2", "world", ":-)" };
+
+void NewArr(string[] arr)
+{
+    Console.Write(" -> ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            Console.Write($"[{arr[i]}]");
+            // Console.Write(",");
+        }
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+PrintArray(array);
+NewArr(array);
